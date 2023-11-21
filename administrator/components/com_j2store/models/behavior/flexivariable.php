15199572de70dd4d498f261feb763eee35d206d4
cartrabbit <hyenas143@gmail.com>
2023-11-21 15:01:42 +0530

    Have fixed the product thumbnail image

--- administrator/components/com_j2store/models/behavior/flexivariable.php
+++ administrator/components/com_j2store/models/behavior/flexivariable.php
@@ -652,9 +652,14 @@ class J2StoreModelProductsBehaviorFlexiVariable extends F0FModelBehavior {
         $return = array();
         $return['variant_id'] = $variant->j2store_variant_id;
         $param_data = $platform->getRegistry($variant->params);
+        $is_main_as_thum = $param_data->get('is_main_as_thum',0);
         $main_image = $param_data->get('variant_main_image','');
+        if($is_main_as_thum){
+            $thumb_image = isset( $main_image ) && !empty( $main_image )  ? $main_image: '';
+        }
         $image_path = JUri::root();
-        $return['main_image'] = isset( $main_image ) && !empty( $main_image ) ? $image_path.$main_image : $image_path.$product->main_image;
+        $return['thumb_image'] = isset( $thumb_image) && !empty( $thumb_image) ? $image_path.$thumb_image : $image_path.$product->thumb_image ;
+        $return['main_image'] = isset( $main_image ) && !empty( $main_image ) ? $image_path.$main_image : $image_path.$product->main_image ;
         $return['sku'] = $variant->sku;
         $return['quantity'] = floatval($quantity);
         $return['price'] = $variant->price;
--- plugins/j2store/app_flexivariable/app_flexivariable/js/flexivariable.js
+++ plugins/j2store/app_flexivariable/app_flexivariable/js/flexivariable.js
@@ -99,9 +99,8 @@ function doFlexiAjaxPrice(product_id, id) {
                         }
                     }
                     if(response.main_image){
-
-                        /*$product.find('.j2store-product-thumb-image-'+product_id).attr("src", response.main_image);
-                        j2store.jQuery('.j2store-product-thumb-image-'+product_id).attr("src", response.main_image);*/
+                        $product.find('.j2store-product-thumb-image-'+product_id).attr("src", response.thumb_image);
+                        j2store.jQuery('.j2store-product-thumb-image-'+product_id).attr("src", response.thumb_image);
                         j2store.jQuery('.j2store-product-main-image-'+product_id).attr("src", response.main_image);
                         $product.find('.j2store-mainimage .j2store-img-responsive').attr("src", response.main_image);
                         $product.find('.j2store-product-additional-images .additional-mainimage').attr("src", response.main_image);
