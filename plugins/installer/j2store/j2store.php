270da3b7e69a90000ff181552a59d9ccfca9db1b
cartrabbit <hyenas143@gmail.com>
2023-11-23 13:10:59 +0530

    Improved update script compatibility with all extensions.

--- plugins/installer/j2store/j2store.php
+++ plugins/installer/j2store/j2store.php
@@ -12,15 +12,19 @@ class PlgInstallerJ2Store extends \Joomla\CMS\Plugin\CMSPlugin
     {
         $domain = 'j2store.org';
         if (strpos($url, $domain) !== false) {
-            if (stripos($url, '/plugin/') !== false) {
-                $element = substr(substr($url, strrpos($url, "/") + 1), 0, -4);
-                $folder = substr($url, strpos($url, '/plugin/') + 8);
-                $type = substr($folder, 0, strpos($folder, '/' . $element));
-                if (!empty($type) && !empty($element)) {
-                    $plugin = $this->getPlugin($type, $element);
-                    if (is_object($plugin) && isset($plugin->params)) {
-                        $params = new \Joomla\Registry\Registry($plugin->params);
-                        $license_key = (array)$params->get('license_key', '');
+            $element = substr(substr($url, strrpos($url, "/") + 1), 0, -4);
+            $pathSegments = explode('/', parse_url($url, PHP_URL_PATH));
+            $position =  array_search($domain, $pathSegments);
+            $type = $pathSegments[$position + 1];
+            if (!empty($type) && !empty($element)) {
+                $plugin = $this->getPlugin($type, $element);
+                if (is_object($plugin) && isset($plugin->params)) {
+                    $params = new \Joomla\Registry\Registry($plugin->params);
+                    $license_key = (array)$params->get('license_key', '');
+                    $is_free = $params->get('is_free',false);
+                    if($is_free){
+                        $url = "https://github.com/j2store/".$element."/releases/download/stable/".$element.".zip";
+                    }else{
                         $baseURL = str_replace('/administrator', '', JURI::base());
                         $api_params = array(
                             'edd_action' => 'get_version',
@@ -46,7 +50,7 @@ class PlgInstallerJ2Store extends \Joomla\CMS\Plugin\CMSPlugin
         }
         $db = \Joomla\CMS\Factory::getDbo();
         $query = $db->getQuery(true);
-        $query->select("*")->from('#__extensions')->where('folder=' . $db->q($type))
+        $query->select("*")->from('#__extensions')->where('type=' . $db->q($type))
             ->where('element=' . $db->q($element));
 
         $db->setQuery($query);
